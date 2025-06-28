21d9f8c9590e1398a5a1ea7ffd4d4f2dbbb85d4b
Bhavya Khatri <bhavya.khatri@gmail.com>
2025-06-28 11:06:23 +0530

    trying

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/mindfulai-frontend/",
+  base: "/mindfulai-frontend/docs/",
   server: {
     host: "::",
     port: 8080,
